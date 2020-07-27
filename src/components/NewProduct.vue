257a178cf04ba00b5fd67d4641b61c0c253e12a4
Ömer Faruk Gürbüz <omer.gurbuz@kletech.com>
2020-07-27 15:52:03 +0300

    #4 ürün ekleme butonu kodlandiş

--- src/components/NewProduct.vue
+++ src/components/NewProduct.vue
@@ -13,19 +13,19 @@
           <div class="card-body">
             <div class="form-group">
               <label>Ürün Adı</label>
-              <input type="text" class="form-control" placeholder="adını giriniz">
+              <input v-model="product.title" type="text" class="form-control" placeholder="adını giriniz">
             </div>
             <div class="row">
               <div class="form-group col-md-6">
                 <label>Ürün Adeti</label>
-                <input type="text" class="form-control" placeholder="adetini giriniz">
+                <input v-model="product.count" type="text" class="form-control" placeholder="adetini giriniz">
               </div>
               <div class="form-group col-md-6">
                 <label>Ürün Fiyatı</label>
-                <input type="text" class="form-control" placeholder="fiyatını giriniz">
+                <input v-model="product.price" type="text" class="form-control" placeholder="fiyatını giriniz">
               </div>
             </div>
-            <button class="btn btn-outline-info btn-block">Ekle!</button>
+            <button @click="addProduct" class="btn btn-outline-info btn-block">Ekle!</button>
           </div>
         </div>
       </div>
@@ -41,6 +41,7 @@
           title:null,
           count:null,
           price:null,
+          totalPrice:null,
           selectedImage: null
         }
       }
@@ -49,6 +50,10 @@
       onChange(e) {
         const file = e.target.files[0];
         this.product.selectedImage = URL.createObjectURL(file);
+      },
+      addProduct(){
+        this.product.totalPrice = this.product.count * this.product.price;
+        console.log(this.product);
       }
     }
   }
